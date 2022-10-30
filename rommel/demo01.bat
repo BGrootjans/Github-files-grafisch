67f401e2dcbd0663a5896191393db37dee73f92b
Bjorn <bjorn.grootjans@student.thomasmore.be>
2022-10-30 13:38:28 +0100

    verwijderd

--- rommel/demo01.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Welkom dit is een voorbeeld script.
-cd "C:\Program Files (x86)\Microsoft\Edge\Application\"
-start msedge.exe --profile-directory=Default www.thomasmore.be
-pause
--- rommel/demo02.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET count=1
-FOR /f "tokens=*" %%G IN ('dir /b') DO (call :subroutine "%%G")
-GOTO :eof
-
-:subroutine
- echo %count%:%1
- set /a count+=0
- GOTO :eof
